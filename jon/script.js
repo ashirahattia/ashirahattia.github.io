4d93312d6c3a6bbf5aa446802b5989c8c95f24be
Angira Shirahatti <ashirahattia@berkeley.edu>
2014-12-18 01:20:25 -0800

    changed js more

--- jon/script.js
+++ jon/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     $(window).load(function(){ 
-        $('#dearjon').delay(800).fadeIn(3000);
+        $('#dearjon').delay(800).fadeIn(2000);
         $('#letter').delay(1200).fadeIn(3000);
     });
 
