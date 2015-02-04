04bf50726c721d5aadb36e6f62e0cd45123d1fa3
Angira Shirahatti <ashirahattia@berkeley.edu>
2015-02-04 23:19:21 +0000

    Feb 4

---
+++ jon/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    $(window).load(function(){ 
+        $('.dearjon').delay(400).fadeIn(2000);
+        $('.letter').delay(400).fadeIn(2000);
+        $('.divider').delay(400).fadeIn(2000);
+    });
+
+});
