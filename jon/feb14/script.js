7088c479357523f30456e10a01cda484ce9b2d6c
Angira Shirahatti <ashirahattia@berkeley.edu>
2015-09-08 01:40:47 -0700

    removed things

--- jon/feb14/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-    $('.heart3').waypoint(function(direction) {
-          $('#one').fadeToggle(1500);
-          $('#two').fadeToggle(1500);
-    }, {offset: '-270'});
-
-});
--- jon/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-    $(window).load(function(){ 
-        $('.dearjon').delay(400).fadeIn(2000);
-        $('.letter').delay(400).fadeIn(2000);
-        $('.divider').delay(400).fadeIn(2000);
-    });
-
-});
